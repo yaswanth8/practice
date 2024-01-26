71b0e1c46c6d3a450520bc50d34a0c4e82a30b1c
yaswanth8 <ykondaveeti@gmail.com>
2024-01-25 19:35:01 -0600

    new

--- src/main/java/com/careerit/jfs/day1/HelloWorld.java
+++ src/main/java/com/careerit/jfs/day1/HelloWorld.java
@@ -1,10 +1,31 @@
 package com.careerit.jfs.day1;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
 
-            System.out.println("Hello");
-    }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum=0;
+        for (int i = 1; i <= n; i++) {
 
+            if(i%3==0 && i%5==0)
+                System.out.println("Fizz & Buzz");
+            else if (i%3==0)
+                System.out.println("Fizz");
+            else if (i%5==0)
+                System.out.println("Buzz");
+            else {
+                System.out.println(i);
+                sum=sum+i;
+                if(sum>(3*n)){
+                    break;
+                }
+            }
+        }
+
+
+    }
 }
